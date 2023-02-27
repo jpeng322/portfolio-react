1a9116e6dda0f94cd67082b124b955edccf40a20
Jacky <jackypengsmail@gmail.com>
2023-02-27 16:30:11 -0500

    Added fade in animation for rest of home section, implemented responsiveness for home

--- portfolio-react/src/pages/Home.jsx
+++ portfolio-react/src/pages/Home.jsx
@@ -10,17 +10,16 @@ const Home = () => {
             <Row className='heading-primary home-row d-flex justify-content-center'>
                 <Col className="home-col-container p-0 m-0" >
                     <Col className="home-col ">
-                        <div className="top-home-heading">
-                            HI THERE,
+                        <div className="top-home-heading-container">
+                            <div className="top-home-heading">HEY THERE,</div>
                         </div>
-                        <div className="bottom-home-heading">
-
-                            I'M JACKY PENG
+                        <div className="bottom-home-heading-container">
+                            <div className='bottom-home-heading'>I'M JACKY PENG</div>
                         </div></Col>
                 </Col>
             </Row>
             <Row className='home-row d-flex justify-content-center'>
-                <Col className="heading-subtext  text-center" xs={12} md={5}>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</Col>
+                <Col className="heading-subtext  text-center" xs={12} md={8} lg={7} xl={5}>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</Col>
             </Row>
             <Row className='home-row d-flex justify-content-center '>
                 <Col className="heading-button text-center p-2" xs={4} md={2}>Contact</Col>
