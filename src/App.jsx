450dd7b64f2c1397bbf3c4017bc4def6fe85bb3e
Jacky <jackypengsmail@gmail.com>
2023-03-15 00:54:31 -0400

    Test 2

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       {/* <LoadPage /> */}
-      {loading ? <LoadPage/> :
+      {loading ? <Stopwatch/> :
         <>
 
           <Stopwatch2 />
