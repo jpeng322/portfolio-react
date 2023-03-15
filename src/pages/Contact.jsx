c2a842add85ccbcf0e8f803d062d9ff850dfeba9
Jacky <jackypengsmail@gmail.com>
2023-03-15 01:09:34 -0400

    Test 8

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,7 +16,7 @@ import emailjs from '@emailjs/browser';
 import FormInput from '../components/ContactComp/FormInput';
 import Media from '../components/Media';
 import SleepingCatSVG from '../components/ContactComp/SleepingCat';
-import Stopwatch2 from '../Stopwatch2';
+// import Stopwatch2 from '../Stopwatch2';
 
 import handleViewport from 'react-in-viewport';
 
