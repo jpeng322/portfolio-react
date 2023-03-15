605e4cc3b1ff2e34e0826b349066e95e10f30a51
Jacky <jackypengsmail@gmail.com>
2023-03-15 00:52:31 -0400

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
   return (
     <div className="App">
       {/* <LoadPage /> */}
-      {loading ? <Stopwatch2/> :
+      {loading ? <LoadPage/> :
         <>
 
           <Stopwatch2 />
--- src/pages/Load.jsx
+++ src/pages/Load.jsx
@@ -1,9 +1,9 @@
-import LoadCat from "../LoadCat";
+import Stopwatch from "../Stopwatch";
 import "../CSS/LoadPage.css"
 const LoadPage = () => {
     return (
         <div className="loader">
-            <LoadCat />
+            <Stopwatch />
         </div>
     );
 }
