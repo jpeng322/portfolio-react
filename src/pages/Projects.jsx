a8489f25beaf2003fb113215cac368acd9883938
Jacky <jackypengsmail@gmail.com>
2023-03-30 15:02:30 -0400

    Added phase 2 project

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,10 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import handleViewport from 'react-in-viewport';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import handleViewport from "react-in-viewport";
 
 //css
-import "../CSS/Projects.css"
+import "../CSS/Projects.css";
 
 ///images
 // import CatSheet from "../outimages/cat-cheetah-sheet.png"
@@ -14,132 +14,166 @@ import "../CSS/Projects.css"
 // import EtchSketch from "../images/etchsketch.jpg"
 
 //components
-import ProjectImage from '../components/ProjectComp/ProjectImage';
+import ProjectImage from "../components/ProjectComp/ProjectImage";
 
 const ProjectHeaderContainer = (props) => {
+  const { inViewport, enterCount, forwardedRef } = props;
 
-    const { inViewport, enterCount, forwardedRef } = props;
+  const animations =
+    inViewport && enterCount === 1
+      ? "animate__animated animate__fadeIn animate__delay-1s"
+      : "";
 
-    const animations = inViewport && enterCount === 1 ? "animate__animated animate__fadeIn animate__delay-1s" : ""
-
-    return (
-        <Row ref={forwardedRef} className={`project-header-container d-flex flex-column text-center ${animations}`}>
-            <Col className="projects-header p-4 ">My recent work</Col>
-            {/* <Col className="projects-header-subtext">Here are a few past design projects I've worked on.</Col> */}
-        </Row>
-    )
-}
-const ProjectImageVP = handleViewport(ProjectImage)
-const ProjectHeaderVP = handleViewport(ProjectHeaderContainer)
+  return (
+    <Row
+      ref={forwardedRef}
+      className={`project-header-container d-flex flex-column text-center ${animations}`}
+    >
+      <Col className="projects-header p-4 ">My recent work</Col>
+      {/* <Col className="projects-header-subtext">Here are a few past design projects I've worked on.</Col> */}
+    </Row>
+  );
+};
+const ProjectImageVP = handleViewport(ProjectImage);
+const ProjectHeaderVP = handleViewport(ProjectHeaderContainer);
 const Projects = () => {
+  return (
+    <Container
+      fluid
+      id="projects-section"
+      className="projects-section d-flex flex-column justify-content-center "
+    >
+      <ProjectHeaderVP />
+      <Row className="d-flex justify-content-center ">
+        <Col
+          className="project-container d-flex "
+          xs={11}
+          md={12}
+          lg={11}
+          xl={12}
+          xxl={12}
+        >
+          <Row className="project-container-row d-flex justify-content-center ">
+            <ProjectImageVP
+              imageSrc="/images/pawpets.PNG"
+              description="A fullstack website that allows users to show off and add pictures of their pets!"
+              textlanguages={
+                <>
+                  <div>
+                    React
+                    <span> • </span>
+                    <span> Express </span>
+                    <span> • </span>
+                    <span> PlanetScale </span>
+                  </div>
+                  <div>
+                    <span> Node </span>
+                    <span> • </span>
+                    <span> React Bootstrap </span>
+                  </div>
+                </>
+              }
+              website="https://pawpets.onrender.com/"
+              github="https://github.com/jpeng322/TKH-Phase2-Project"
+            />
 
-    return (
-        <Container fluid id="projects-section" className="projects-section d-flex flex-column justify-content-center ">
-            <ProjectHeaderVP />
-            <Row className="d-flex justify-content-center ">
-                <Col className="project-container d-flex " xs={11} md={12} lg={11} xl={12} xxl={12}>
-                    <Row className="project-container-row d-flex justify-content-center ">
-
-                        <ProjectImageVP
-                            imageSrc="/images/cat-cheetah-sheet.PNG"
-                            // imageSrc={CatSheet}
-                            description="A responsive website that allows users to find information
+            <ProjectImageVP
+              imageSrc="/images/cat-cheetah-sheet.PNG"
+              description="A responsive website that allows users to find information
                             on numerous cat breeds."
-                            textlanguages=
-                            {<>
-                                React
-                                <span> • </span>
-                                <span> React Bootstrap </span>
-                            </>}
-                            website="https://cat-cheetah-sheet.onrender.com/"
-                            github="https://github.com/jpeng322/cat-app-react-router/tree/main/cat-app"
-                        />
+              textlanguages={
+                <>
+                  React
+                  <span> • </span>
+                  <span> React Bootstrap </span>
+                </>
+              }
+              website="https://cat-cheetah-sheet.onrender.com/"
+              github="https://github.com/jpeng322/cat-app-react-router/tree/main/cat-app"
+            />
 
-                        <ProjectImageVP
-                            imageSrc="/images/journal-app.PNG"
-                            // imageSrc={JournalApp}
-                            description="A full-stack journal application that allows the user to create an account and save daily
+            <ProjectImageVP
+              imageSrc="/images/journal-app.PNG"
+              description="A full-stack journal application that allows the user to create an account and save daily
                             notes. "
-                            textlanguages=
-                            {<>
-                                <div>
-                                    React
-                                    <span> • </span>
-                                    <span> Express </span>
-                                    <span> • </span>
-                                    <span> MongoDB </span>
-                                </div>
-                                <div>
-                                    <span> Node </span>
-                                    <span> • </span>
-                                    <span> React Bootstrap </span>
-                                </div>
-                            </>}
-                            website="https://my-journal-app-7sls.onrender.com"
-                            github="https://github.com/jpeng322/journal_react"
-                        />
+              textlanguages={
+                <>
+                  <div>
+                    React
+                    <span> • </span>
+                    <span> Express </span>
+                    <span> • </span>
+                    <span> MongoDB </span>
+                  </div>
+                  <div>
+                    <span> Node </span>
+                    <span> • </span>
+                    <span> React Bootstrap </span>
+                  </div>
+                </>
+              }
+              website="https://my-journal-app-7sls.onrender.com"
+              github="https://github.com/jpeng322/journal_react"
+            />
 
-                        <ProjectImageVP
-                            imageSrc="/images/pokedex.PNG"
-                            // imageSrc={Pokedex}
-                            description="Dynamic Pokedex that searches and displays statistics of a specific
+            <ProjectImageVP
+              imageSrc="/images/pokedex.PNG"
+              description="Dynamic Pokedex that searches and displays statistics of a specific
                             pokemon."
-                            textlanguages=
-                            {<>
-                                JS
-                                <span> •  </span>
-                                <span> CSS </span>
-                                <span> • </span>
-                                <span> HTML </span>
-                            </>}
-                            website="https://pokedex-n7lj.onrender.com/"
-                            github="https://github.com/jpeng322/TKH-pokedex"
-                        />
+              textlanguages={
+                <>
+                  JS
+                  <span> • </span>
+                  <span> CSS </span>
+                  <span> • </span>
+                  <span> HTML </span>
+                </>
+              }
+              website="https://pokedex-n7lj.onrender.com/"
+              github="https://github.com/jpeng322/TKH-pokedex"
+            />
 
-
-                        <ProjectImageVP
-                            imageSrc="/images/tic-tac-toe-pic.PNG"
-                            // imageSrc={TicTacToe}
-                            description="A two-player Squid Games themed Tic-Tac-Toe game with custom player names, 
+            <ProjectImageVP
+              imageSrc="/images/tic-tac-toe-pic.PNG"
+              description="A two-player Squid Games themed Tic-Tac-Toe game with custom player names, 
                             marker selection, and move
                             indicators."
-                            textlanguages=
-                            {<>
-                                JS
-                                <span> •  </span>
-                                <span> CSS </span>
-                                <span> • </span>
-                                <span> HTML </span>
-                            </>}
-                            website="https://jpeng322.github.io/TKH-Tic-Tac-Toe/"
-                            github="https://github.com/jpeng322/TKH-Tic-Tac-Toe"
-                        />
+              textlanguages={
+                <>
+                  JS
+                  <span> • </span>
+                  <span> CSS </span>
+                  <span> • </span>
+                  <span> HTML </span>
+                </>
+              }
+              website="https://jpeng322.github.io/TKH-Tic-Tac-Toe/"
+              github="https://github.com/jpeng322/TKH-Tic-Tac-Toe"
+            />
 
+            <ProjectImageVP
+              imageSrc="/images/etchsketch.jpg"
+              // imageSrc={EtchSketch}
+              description="An online rendition of the Etch A Sketch drawing toy with rainbow, erasure, and color selection modes."
+              textlanguages={
+                <>
+                  JS
+                  <span> • </span>
+                  <span> CSS </span>
+                  <span> • </span>
+                  <span> HTML </span>
+                </>
+              }
+              website="https://jpeng322.github.io/Etch-a-Sketch-Project/"
+              github="https://github.com/jpeng322/Etch-a-Sketch-Project"
+            />
+          </Row>
+        </Col>
 
-                        <ProjectImageVP
-                            imageSrc="/images/etchsketch.jpg"
-                            // imageSrc={EtchSketch}
-                            description="An online rendition of the Etch A Sketch drawing toy with rainbow, erasure, and color selection modes."
-                            textlanguages=
-                            {<>
-                                JS
-                                <span> •  </span>
-                                <span> CSS </span>
-                                <span> • </span>
-                                <span> HTML </span>
-                            </>}
-                            website="https://jpeng322.github.io/Etch-a-Sketch-Project/"
-                            github="https://github.com/jpeng322/Etch-a-Sketch-Project"
-                        />
-
-                    </Row>
-                </Col>
-
-
-                {/* <Col></Col> */}
-            </Row>
-        </Container>
-    );
-}
+        {/* <Col></Col> */}
+      </Row>
+    </Container>
+  );
+};
 
 export default Projects;
